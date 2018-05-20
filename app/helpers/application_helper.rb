489f33739daab04becc8df6a05088924dbd448a0
EC2 Default User <ec2-user@ip-172-31-36-42.us-east-2.compute.internal>
2018-05-20 19:13:30 +0000

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+    def full_title(page_title)
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            "#{page_title} | #{base_title}"
+        end
+    end
+    def bootstrap_class_for(flash_type)
+        case flash_type
+        when :success
+            "alert-success"
+        when :error
+            "alert-danger"
+        when :alert
+            "alert-warning"
+        when :notice
+            "alert-info"
+        else
+            flash_type.to_s
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
